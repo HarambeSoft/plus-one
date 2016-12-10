e1e850360a5635cd8ad5dcea29dcca44bd9700f1
isamert <isamertgurbuz@gmail.com>
2016-12-10 21:40:45 +0300

    move gcm to fcm

---
+++ app/src/main/java/harambesoft/com/plusone/notification/FirebaseRegister.java
@@ -0,0 +1,30 @@
+package harambesoft.com.plusone.notification;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by isa on 10.12.2016.
+ */
+
+public class FirebaseRegister extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+    }
+}
