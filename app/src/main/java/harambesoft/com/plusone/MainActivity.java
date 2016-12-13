4bfc9386e1fb557cd456e308c67d9eaba1bb8b7f
isamert <isamertgurbuz@gmail.com>
2016-12-13 05:04:27 +0300

    clean imports

--- app/src/main/java/harambesoft/com/plusone/MainActivity.java
+++ app/src/main/java/harambesoft/com/plusone/MainActivity.java
@@ -1,17 +1,10 @@
 package harambesoft.com.plusone;
 
-import android.*;
-import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,9 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import android.support.v4.app.FragmentManager;
-
 import android.widget.TextView;
 
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -68,6 +59,7 @@ public class MainActivity extends AppCompatActivity
         assignWidgets();
         checkUserLogin();
 
+        //
         startService(new Intent(this, LocationTrackerService.class));
     }
 
