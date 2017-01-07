3c3a0cf4b1ffda4c0eba0240a9e72970c333560a
Alpccelik <alprslnclk@gmail.com>
2017-01-07 16:49:45 +0300

    shared pref deleted

--- app/src/main/java/harambesoft/com/plusone/fragments/SettingsFragment.java
+++ app/src/main/java/harambesoft/com/plusone/fragments/SettingsFragment.java
@@ -1,7 +1,5 @@
 package harambesoft.com.plusone.fragments;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.widget.SwitchCompat;
@@ -17,15 +15,9 @@ import harambesoft.com.plusone.R;
  */
 
 public class SettingsFragment extends Fragment {
-    private static final String TAG = "MyActivity";
-
 
     @BindView(R.id.swtichnot)
     SwitchCompat swtichnot;
-    SharedPreferences preferences = this.getActivity().getSharedPreferences("setting", Context.MODE_PRIVATE);
-    String offnot = "App.settings().edit().putBoolean(showNotifications, false).commit();" + " FirebaseMessaging.getInstance().unsubscribeFromTopic(CurrentUser.notificationTopic());";
-    String onnot = "App.settings().edit().putBoolean(showNotifications, true).commit();" + "FirebaseMessaging.getInstance().subscribeToTopic(CurrentUser.notificationTopic());";
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
