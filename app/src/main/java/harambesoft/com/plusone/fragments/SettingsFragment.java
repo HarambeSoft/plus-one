f51aac5b8559eef18d787de5724167922c30b884
Alpccelik <alprslnclk@gmail.com>
2017-01-07 16:10:49 +0300

    shared pref. added punto size updated

--- app/src/main/java/harambesoft/com/plusone/fragments/SettingsFragment.java
+++ app/src/main/java/harambesoft/com/plusone/fragments/SettingsFragment.java
@@ -1,12 +1,15 @@
 package harambesoft.com.plusone.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
 import harambesoft.com.plusone.R;
 
 /**
@@ -14,16 +17,22 @@ import harambesoft.com.plusone.R;
  */
 
 public class SettingsFragment extends Fragment {
+    private static final String TAG = "MyActivity";
+
+
+    @BindView(R.id.swtichnot)
+    SwitchCompat swtichnot;
+    SharedPreferences preferences = this.getActivity().getSharedPreferences("setting", Context.MODE_PRIVATE);
+    String offnot = "App.settings().edit().putBoolean(showNotifications, false).commit();" + " FirebaseMessaging.getInstance().unsubscribeFromTopic(CurrentUser.notificationTopic());";
+    String onnot = "App.settings().edit().putBoolean(showNotifications, true).commit();" + "FirebaseMessaging.getInstance().subscribeToTopic(CurrentUser.notificationTopic());";
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
-    }
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
     }
 }
+
